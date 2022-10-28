ed0ea76584876e1b12640bb82b52fccf223389eb
Yingjie Jia <julieyingjie@gmail.com>
2022-10-27 21:47:46 -0400

    Add the enroll method

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -7,13 +7,13 @@ public class Student {
     private String lastName;
     private int gradeYear;
     private String studentID;
-    private String courses;
-    private int tuitionBalance;
+    private String courses = null;
+    private int tuitionBalance = 0;
     private static int costOfCourse = 600;
     private static int id = 1000;
 
     // constructor : prompt user to enter student's name and year
-    public Student(){
+    public Student() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter student first name: ");
         this.firstName = in.nextLine();
@@ -21,8 +21,8 @@ public class Student {
         System.out.println("Enter student last name: ");
         this.lastName = in.nextLine();
 
-        System.out.println("Enter student grade level: "+
-                            "\n1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior");
+        System.out.println("Enter student grade level: " +
+                "\n1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior");
         this.gradeYear = in.nextInt();
 
         setStudentID();
@@ -32,13 +32,33 @@ public class Student {
     }
 
     // generate an ID
-    private void setStudentID(){
+    private void setStudentID() {
         id++;
         // Grade level + ID
-        this.studentID = gradeYear +  "" + id;
+        this.studentID = gradeYear + "" + id;
     }
 
     // Enroll in courses
+    public void enroll() {
+        // Get inside a loop, users hits 0
+        do {
+            System.out.print("Enter course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            }
+            else {
+                System.out.println("Break!");
+                break;
+            }
+        }while ( 1 != 0);
+
+
+        System.out.println("Enrolled In: " + courses);
+        System.out.println("Tuition balance: " + tuitionBalance);
+    }
 
     // View balance
 
--- src/main/java/org/example/StudentDatabaseApp.java
+++ src/main/java/org/example/StudentDatabaseApp.java
@@ -3,6 +3,9 @@ package org.example;
 public class StudentDatabaseApp {
     public static void main(String[] args) {
         Student stu1 = new Student();
+        stu1.enroll();
+
+
         // Ask how many new users we want to add
 
         // Create n number of new students
