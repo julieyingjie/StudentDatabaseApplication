aef6768b7f1503a13d1d803a6d7435196ea7fc71
Yingjie Jia <julieyingjie@gmail.com>
2022-10-27 21:24:05 -0400

    Add the constructor, setStudentID method

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,2 +1,48 @@
-package org.example;public class Student {
+package org.example;
+
+import java.util.Scanner;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private String studentID;
+    private String courses;
+    private int tuitionBalance;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
+
+    // constructor : prompt user to enter student's name and year
+    public Student(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("Enter student grade level: "+
+                            "\n1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior");
+        this.gradeYear = in.nextInt();
+
+        setStudentID();
+
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+
+    }
+
+    // generate an ID
+    private void setStudentID(){
+        id++;
+        // Grade level + ID
+        this.studentID = gradeYear +  "" + id;
+    }
+
+    // Enroll in courses
+
+    // View balance
+
+    // Pay tuition
+
+    // Show status
 }
--- src/main/java/org/example/StudentDatabaseApp.java
+++ src/main/java/org/example/StudentDatabaseApp.java
@@ -1,2 +1,10 @@
-package org.example;public class StudentDatabaseApp {
+package org.example;
+
+public class StudentDatabaseApp {
+    public static void main(String[] args) {
+        Student stu1 = new Student();
+        // Ask how many new users we want to add
+
+        // Create n number of new students
+    }
 }
